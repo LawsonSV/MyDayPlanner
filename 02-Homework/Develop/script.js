c57812cfafa334cdb5a3e34c3cfbac5c9e95148c
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-26 19:24:45 -0700

    added rows for each hour, added text area and save button to each row, added dynamic current day and time at the top under the title

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,7 @@
+var today = moment().format("dddd MMM Do YYYY, h:mm a");
+let dayText = $("#currentDay");
+let timeBlock = $(".time-block");
+
+dayText.append(today)
+
+//timeBlock.css("background-color", "red")
